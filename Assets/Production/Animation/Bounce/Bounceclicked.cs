5d0f9ec808bff71b984f8e532bf47333499b7c67
Philibert <gael.p40@outlook.fr>
2026-03-23 15:17:59 +0100

    update animation not begin and count click message in debug A vous la teams

--- Assets/Production/Animation/Bounce/Bounceclicked.cs
+++ Assets/Production/Animation/Bounce/Bounceclicked.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 public class Bounceclicked : MonoBehaviour
 {
- [SerializeField]
-    Camera cam;
+    [SerializeField] Camera cam;
 
     Animator anim;
+    public int clickCount = 0; 
 
     void Start()
     {
@@ -15,23 +15,41 @@ public class Bounceclicked : MonoBehaviour
 
     void Update()
     {
-        if(cam != null)
+        if (cam != null)
         {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
+            if (Input.GetMouseButtonDown(0))
             {
- 
-                if (hit.transform == transform)
-                {
-                    Debug.Log("AAAAAAHHHHH");
+                Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
 
-                    anim.SetTrigger("Bounceclicked");                   
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.transform == transform)
+                    {
+                        clickCount++; 
+                        Debug.Log(gameObject.name + " clics : " + clickCount);
+
+                        anim.SetTrigger("Bounceclicked");
+
+                        CheckActions();
+                    }
                 }
             }
-        }    
+        }
     }
+
+    void CheckActions()
+    {
+        if (clickCount == 3)
+        {
+            Debug.Log("3 clics !");
+            transform.localScale *= 1.2f;
+        }
+
+        if (clickCount == 5)
+        {
+            Debug.Log("5 clics !");
+            GetComponent<Renderer>().material.color = Color.red;
+        }
     }
 }
